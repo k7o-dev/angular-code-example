b7203dc974308d223c2723a8949e7821abb3a9cd
k7o <k7o.dev@gmail.com>
2022-09-18 01:36:00 +0900

    プロジェクト名を変更

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-sample'),
+      dir: require('path').join(__dirname, './coverage/angular-code-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
